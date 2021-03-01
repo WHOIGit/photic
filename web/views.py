320d7b5c53a29966d22ce122a0873483cd555914
Mike Chagnon <mike@kaimika.com>
2021-03-01 17:43:42 +0100

    auto-expand accordian if filters are selected

--- web/views.py
+++ web/views.py
@@ -12,12 +12,17 @@ def index(request):
 
     labels = Label.objects.all()
 
+    # TODO: Hook up to "winning" label filter"
     if request.GET.get("label"):
         annotations = annotations.filter(label__name=request.GET.get("label"))
 
+    # TODO: Hook up annotator filter
+
+    is_filtered = request.GET.get("label") or request.GET.get("annotator")
 
     return render(request, "web/index.html", {
         "annotations": annotations,
         "annotation_users": annotation_users,
         "labels": labels,
+        "is_filtered": is_filtered,
     })
