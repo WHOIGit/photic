03ef900faa7fad17315a62b6cad08858826e8bdf
Mike Chagnon <mike@kaimika.com>
2021-03-01 17:06:50 -0500

    Demo updates (#9)
    * disabled start/end date filters; populate action label dropdown
    
    * auto-expand accordian if filters are selected
    
    * fill in collection dropdown
    
    * updated copyright in footer

--- web/views.py
+++ web/views.py
@@ -1,23 +1,28 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-from core.models import Annotation
-from core.models import Label
-from django.contrib.auth.models import User
+from core.models import Annotation, Label, ImageCollection
 
 def index(request):
     # TODO: Group by ROI...
     annotations = Annotation.objects.all()
-
     annotation_users = User.objects.all()
+    collections = ImageCollection.objects.all()
 
     labels = Label.objects.all()
 
+    # TODO: Hook up to "winning" label filter"
     if request.GET.get("label"):
         annotations = annotations.filter(label__name=request.GET.get("label"))
 
+    # TODO: Hook up annotator filter
+
+    is_filtered = request.GET.get("label") or request.GET.get("annotator")
 
     return render(request, "web/index.html", {
         "annotations": annotations,
         "annotation_users": annotation_users,
         "labels": labels,
+        "is_filtered": is_filtered,
+        "collections": collections,
     })
