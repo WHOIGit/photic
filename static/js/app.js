62b77c6f941c8b99eefc174d401319aed3c33f62
Joe Futrelle <joefutrelle@gmail.com>
2021-03-23 13:00:11 -0400

    Adding generated/modified assets

--- static/js/app.js
+++ static/js/app.js
@@ -71353,7 +71353,7 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).foundation();
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/vagrant/dev/Photic/assets/src/js/app.js */"./src/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\jfutrelle\Documents\GitHub\photic\assets\src\js\app.js */"./src/js/app.js");
 
 
 /***/ })
