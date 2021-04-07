b48a370133f49ff5b6003de5202b3ebac4982ed3
kaimikacolin <colin@kaimika.com>
2021-04-06 22:13:46 -0600

    remove unused /home

--- web/views.py
+++ web/views.py
@@ -13,18 +13,6 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def home(request):
-    numbers_list = range(1, 1000)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(numbers_list, 20)
-    try:
-        numbers = paginator.page(page)
-    except PageNotAnInteger:
-        numbers = paginator.page(1)
-    except EmptyPage:
-        numbers = paginator.page(paginator.num_pages)
-    return render(request, 'web/home.html', {'numbers': numbers})
-
 def index(request):
     annotation_users = User.objects.all()
     collections = ImageCollection.objects.all()
