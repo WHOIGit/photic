6bc30bcb1d0c965422123df8622e986138d299e7
kaimikacolin <colin@kaimika.com>
2021-04-08 16:26:01 -0600

    add prev/next label buttons

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -228,6 +228,24 @@ function get_labels_callback(r){
         }
     }
 }
+$("#next_label").on('click', nextLabel);
+$("#prev_label").on('click', prevLabel);
+function nextLabel(){
+    let $next = $('#apply_label_select option:selected').next();
+    if($next.length==0){
+        $next = $('#apply_label_select option').first();
+    }
+    $next.prop('selected', true).change();
+}
+
+function prevLabel(){
+    let $prev = $('#apply_label_select option:selected').prev();
+    if($prev.length==0){
+        $prev = $('#apply_label_select option').last();
+    }
+    $prev.prop('selected', true).change();
+    
+}
 
 $("#unhide-last-form").on('submit', function(ev){
     ev.preventDefault();
